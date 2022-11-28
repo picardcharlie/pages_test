3a27c5cfaab80b589865dc2654b3bcfc72761daf
picardcharlie <picardcharlie@gmail.com>
2022-11-28 14:43:05 -0600

    first

---
+++ script.js
@@ -0,0 +1,20 @@
+writeRandomQuote = function () {
+	let quotes = [
+		"What did you say? Breeding lamp?",
+		"It's ok, we're cousins.",
+		"You ever notice that girls are cute?",
+		"And when I'm done and dead, you better empty the grease trap!"
+	]
+
+	let rand = Math.floor(Math.random()*quotes.lenth);
+	document.getElementById("quotes").innerText = quotes[rand];
+}
+
+function copyDate() {
+	let footer = document.getElementById("copyright")
+	let copyrightText = "copyright [YOUR_NAME] " + new Date().getFullYear()
+	footer.innerText = copyrightText
+}
+
+copyDate();
+writeRandomQuote();
